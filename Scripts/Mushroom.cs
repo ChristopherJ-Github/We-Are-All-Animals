0eea4cb654dc09675a915199e8e5bcda165f95b7
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-07-10 05:00:03 -0400

    Ryan birthday added

---
+++ Scripts/Mushroom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mushroom : MonoBehaviour {
+
+	void Awake () {
+
+		initScale = transform.localScale.x;
+	}
+
+	void Update () {
+
+		UpdateScale ();
+	}
+
+	private float initScale;
+	public float minScale;
+	public float maxScale = 1;
+	void UpdateScale () {
+
+		float scaleAmount = Mathf.InverseLerp(0.24f, 0.85f, SunControl.instance.posInDay);
+		float minScale = this.minScale != 0 ? this.minScale/100 : initScale;
+		float currentScalar = Mathf.Lerp (minScale, maxScale/100, scaleAmount);
+		transform.localScale = Vector3.one * currentScalar;
+	}
+
+}
