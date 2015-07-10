c011d3163f6cd40d6cdb08018b81d03fe3f76ca4
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-07-10 03:09:18 -0400

    Yifat birthday added

---
+++ Scripts/MonsterSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterSpawner : MonoBehaviour {
+	
+	void OnEnable () {
+
+		SceneManager.instance.OnNewHour += SwitchAnimation;
+		SwitchAnimation ();
+	}
+	
+	public GameObject[] animations;
+	private GameObject currentAnimation;
+	void SwitchAnimation () {
+
+		int randomIndex = Random.Range (0, animations.Length);
+		currentAnimation = animations [randomIndex];
+		currentAnimation.SetActive (true);
+		foreach (GameObject animation in animations) {
+			if (animation != currentAnimation) 
+				animation.SetActive(false);
+		}
+	}
+
+	void OnDisable () {
+
+		foreach (GameObject animation in animations) 
+			animation.SetActive(false);
+	}
+}
--- Scripts/Tester.cs
+++ Scripts/Tester.cs
@@ -9,9 +9,13 @@ public class Tester : Singleton <Tester> {
 	}
 
 	public static bool test = true;
+	public static bool buttonPressed;
 	void CheckForInput () {
 
 		if (Input.GetKeyDown(KeyCode.F)) 
 			test = !test;
+		buttonPressed = false;
+		if (Input.GetKeyDown(KeyCode.F)) 
+			buttonPressed = true;
 	}
 }
