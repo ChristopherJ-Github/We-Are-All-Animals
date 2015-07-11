2a823c04f708f410f88bb3eda223d57d234bf64a
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-07-11 01:46:36 -0400

    Lili birthday complete

--- Scripts/Hover.cs
+++ Scripts/Hover.cs
@@ -3,15 +3,61 @@ using System.Collections;
 
 public class Hover : MonoBehaviour {
 
-	public bool waterLily;
+	public enum LilyType {waterLily, lilyPad}
+	public LilyType lilyType;
 	void Start () {
 
+		InitValues ();
+		RandomizeValues ();
+	}
+
+	void InitValues () {
+
+		if (lilyType == LilyType.waterLily) {
+			WaterLilyProperties waterLilyProperties = LilyManager.instance.waterLilyProperties;
+			originalHeight = waterLilyProperties.originalHeight;
+			speed = waterLilyProperties.speed;
+			height = waterLilyProperties.height;
+			hoverAnimation = waterLilyProperties.hoverAnimation;
+			transform.localScale = Vector3.one * waterLilyProperties.scale;
+		}
+		if (lilyType == LilyType.lilyPad) {
+			LilyPadProperties lilyPadProperties = LilyManager.instance.lilyPadProperties;
+			originalHeight = lilyPadProperties.originalHeight;
+			speed = lilyPadProperties.speed;
+			height = lilyPadProperties.height;
+			hoverAnimation = lilyPadProperties.hoverAnimation;
+		}
 		originalPos = transform.position;
-		originalPos.y = originalHeight != 0 ? originalHeight : originalPos.y;
+		originalPos.y = originalHeight;
+	}
+
+	void RandomizeValues() {
+
+		if (lilyType == LilyType.waterLily) {
+			WaterLilyProperties waterLilyProperties = LilyManager.instance.waterLilyProperties;
+			RandomizeTilt(waterLilyProperties);
+			renderer.material = LilyManager.instance.GetRandomMaterial ();
+		}
+		if (lilyType == LilyType.lilyPad) {
+			LilyPadProperties lilyPadProperties = LilyManager.instance.lilyPadProperties;
+			float scale = Mathf.Lerp (lilyPadProperties.minScale, lilyPadProperties.maxScale, Random.value);
+			transform.localScale = Vector3.one * scale;
+			Color tint = Random.value > 0.5f ? lilyPadProperties.tint1 : lilyPadProperties.tint2;
+			renderer.material.color = tint;
+		}
 		hoverPosition = Random.value;
 		RandomizeRotation ();
-		if (waterLily)
-			renderer.material = WaterLilyManager.instance.GetRandomMaterial ();
+	}
+
+	void RandomizeTilt (WaterLilyProperties waterLilyProperties) {
+
+		float xTilt = Random.Range (-waterLilyProperties.maxTilt, waterLilyProperties.maxTilt);
+		float zTilt = Random.Range (-waterLilyProperties.maxTilt, waterLilyProperties.maxTilt);
+		Vector3 newEuler = transform.eulerAngles;
+		newEuler.x += xTilt;
+		newEuler.z += zTilt;
+		transform.rotation = Quaternion.Euler (newEuler);
 	}
 
 	void RandomizeRotation () {
@@ -23,15 +69,15 @@ public class Hover : MonoBehaviour {
 	}
 	
 	void Update () {
-		
+
 		UpdatePosition ();
 	}
 
 	private float hoverPosition;
 	private Vector3 originalPos;
-	public float originalHeight;
-	public float speed, height;
-	public AnimationCurve hoverAnimation;
+	private float originalHeight;
+	private float speed, height;
+	private AnimationCurve hoverAnimation;
 	void UpdatePosition () {
 
 		hoverPosition = (hoverPosition + speed * Time.deltaTime) % 1;
---
+++ Scripts/LilyManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class WaterLilyProperties {
+
+	public float originalHeight;
+	public float speed, height;
+	public AnimationCurve hoverAnimation;
+	public float maxTilt;
+	public float scale;
+}
+
+[System.Serializable]
+public class LilyPadProperties {
+
+	public float originalHeight;
+	public float speed, height;
+	public AnimationCurve hoverAnimation;
+	public float minScale, maxScale;
+	public Color tint1, tint2;
+}
+
+public class LilyManager : Singleton<LilyManager> {
+
+	public WaterLilyProperties waterLilyProperties;
+	public LilyPadProperties lilyPadProperties;
+	public Material[] materials;
+	public Material GetRandomMaterial () {
+
+		int materialIndex = Random.Range (0, materials.Length);
+		return materials[materialIndex];
+	}
+}
