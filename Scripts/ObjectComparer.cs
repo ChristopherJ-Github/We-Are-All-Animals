9e96fa2b42b35384d408d076467d93c8629f5b15
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-07-26 03:57:14 -0400

    Shadows updated in the evening

---
+++ Scripts/ObjectComparer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectComparer : MonoBehaviour {
+
+	public GameObject objectA, objectB;
+
+	void Update () {
+
+		objectA.SetActive (Tester.test);
+		objectB.SetActive (!objectA.activeSelf);
+	}
+}
