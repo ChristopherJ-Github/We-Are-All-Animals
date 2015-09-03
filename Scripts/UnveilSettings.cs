25b4244b85ebfab4db5af6f631fc77ee1184f58f
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-09-03 16:20:24 -0400

    Animal spawning added to UnveilSettings

--- Scripts/UnveilSettings.cs
+++ Scripts/UnveilSettings.cs
@@ -1,14 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
+[System.Serializable]
+public class AnimalInfo {
+
+	public AnimalAnimator animalAnimator;
+	public Vector2 spawnTime;
+	public DateTime dateTime;
+	[HideInInspector] public bool spawned;
+
+}
 
 public class UnveilSettings : MonoBehaviour {
 	
 	void Start () {
 
 		SkyManager.instance.SetPhaseTimes (12, 15, 19.5f, 20.25f);
+		InitializeDateTimes ();
+	}
+
+	void InitializeDateTimes () {
+
+		for (int i = 0; i < animals.Length; i++) {
+			Vector2 spawnTime = animals[i].spawnTime;
+			DateTime dateTime = new DateTime(SceneManager.currentDate.Year, 
+			                                 9, 17, (int)spawnTime.x, (int)spawnTime.y, 0);
+			animals[i].dateTime = dateTime;
+		}
 	}
 
 	void Update () {
-	
+
+		MakeAnimalSpawnAttempt ();
+	}
+
+	public AnimalInfo[] animals;
+	void MakeAnimalSpawnAttempt () {
+
+		foreach (AnimalInfo animalInfo in animals) {
+			if (animalInfo.spawned) continue;
+			if (animalInfo.dateTime >= SceneManager.realDate) {
+				AnimationSpawner.instance.Spawn(animalInfo.animalAnimator);
+				animalInfo.spawned = true;
+			}
+		}
 	}
 }
