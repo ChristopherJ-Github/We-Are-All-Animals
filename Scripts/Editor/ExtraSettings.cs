5b7ead57785e3dc09bfee7b57d0f93c5dac9c47c
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-11-06 13:27:51 -0500

    Scene name updated. Multi thread rendering disabled

---
+++ Scripts/Editor/ExtraSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+public class ExtraSettings {
+
+	[MenuItem("Extra Settings/Enable MT Rendering")]
+	static void EnableMTRendering() {
+
+		UnityEditor.PlayerSettings.MTRendering = true;
+	}
+
+	[MenuItem("Extra Settings/Disable MT Rendering")]
+	static void DisableMTRendering() {
+
+		UnityEditor.PlayerSettings.MTRendering = false;
+	}
+
+	[MenuItem("Extra Settings/Show MT Rendering Status")]
+	static void ShowMTRenderingStatus() {
+
+		UnityEditor.EditorUtility.DisplayDialog ("MT Rendering Status",
+		                                        (UnityEditor.PlayerSettings.MTRendering)
+		                                        ? "Multi-Threading Rendering is currently ON"
+		                                        : "Multi-Threading Rendering is currently OFF"
+		                                        , "OK");
+	}
+}
